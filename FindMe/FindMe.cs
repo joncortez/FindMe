736b6899b05f09c55cc325d69b111608e0a6058f
Jon Cortez <jcortez@gmail.com>
2016-03-01 00:18:41 -0800

    Initial commit.

---
+++ FindMe/FindMe.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+
+namespace FindMe
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var mainPage = new SignInPage();
+            var navPage = new NavigationPage(mainPage)
+            {
+                BarTextColor = Color.White,
+                BarBackgroundColor = Color.FromHex("#747474")
+            };
+            MainPage = navPage;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
+
---
+++ FindMe/Views/CustomRenderers.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace FindMe.Views
+{
+    public class LabelWithCustomFont : Label { }
+    public class ButtonWithCustomFont : Button { }
+    public class PageKeyboardAware : ContentPage { }
+}
+
